561a9236247f6d77bae82c0f5c910db221fece17
manab oz <staff@oz-edu.com>
2022-01-11 21:33:43 +0900

    Initial Commit

---
+++ hayulu-dining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hayulu-dining
+//
+//  Created by 辻大地 on 2022/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
